531815783347f84ed9cd512d7a7ff035566dae40
Stephen Gaito <stephen@perceptisys.co.uk>
2016-02-10 14:08:36 +0000

    initial pass at creation of scribble documents and runing rackUnit tests

---
+++ racket/pkgs/diSimp/tests/interpreter.rkt
@@ -0,0 +1,25 @@
+#lang racket/base
+
+(require rackunit)
+(require rackunit/text-ui)
+
+(define all-tests
+  (test-suite
+   "Tests for interpreter.rkt"
+    (check-equal? 1 1 "simple test")
+    (test-case
+     "List has length 4 and all elements even"
+      (let ([lst (list 2 4 6 9)])
+        (check = (length lst) 4)
+        (for-each
+          (lambda (elt)
+            (check-pred even? elt)
+          )
+          lst
+        )
+      )
+    )
+  )
+)
+
+(run-tests all-tests)
