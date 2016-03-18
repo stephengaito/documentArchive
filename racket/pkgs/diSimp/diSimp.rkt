bd70803e5c95450bb173ca71292c4a1c3df13d6e
Stephen Gaito <stephen@perceptisys.co.uk>
2016-03-18 16:03:47 +0000

    stubbed out initial diSimp language as a restrictive sub language of racket

--- racket/pkgs/diSimp/diSimp.rkt
+++ racket/pkgs/diSimp/diSimp.rkt
@@ -1,3 +1,19 @@
-#lang racket
+#lang racket/base
 
-(provide (all-from-out racket))
+#|
+;; This form opens everything out and is TOO permissive
+;;
+(provide (except-out (all-from-out racket/base) #%module-begin)
+         (rename-out [#%plain-module-begin #%module-begin])
+)
+|#
+
+
+;; This form, if it works, is very restrictive
+;;
+(provide 
+  (rename-out [#%plain-module-begin #%module-begin])
+  #%app #%datum #%top
+  #%require #%provide
+  displayln
+)
