f11fd0328554b0a732d5cc1f40c167b215e47bbd
Stephen Gaito <stephen@perceptisys.co.uk>
2016-03-18 17:09:05 +0000

    cleaned up diSimp language definition

--- racket/pkgs/diSimp/main.rkt
+++ racket/pkgs/diSimp/main.rkt
@@ -1,5 +1,22 @@
 #lang racket/base
 
-(require "diSimp.rkt")
-(provide (all-from-out "diSimp.rkt"))
+;; This is the main entry point for the diSimp module/language
+
+#|
+;; This form opens everything out and is TOO permissive
+;;
+(provide (except-out (all-from-out racket/base) #%module-begin)
+         (rename-out [#%plain-module-begin #%module-begin])
+)
+|#
+
+
+;; This form, if it works, is very restrictive
+;;
+(provide 
+  (rename-out [#%plain-module-begin #%module-begin])
+  #%app #%datum #%top
+  #%require #%provide
+  displayln
+)
 
