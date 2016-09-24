8367352038294bf77e1cfe161d484196fe66907b
Stephen Gaito <stephen@perceptisys.co.uk>
2016-09-24 06:33:06 +0100

    begun writing specs for diSimpInterpreter

--- racket/pkgs/diSimpInterpreter/specs/functional/diSimpSpec.rkt
+++ racket/pkgs/diSimpInterpreter/specs/functional/diSimpSpec.rkt
@@ -15,8 +15,24 @@
 
 (define all-tests
   (test-suite "Describe the specification of diSimp"
+
     (test-case "Show that diSimp is a function from lists to lists"
       (check-true (list? (diSimp '())))
+    )
+
+    (test-case "Noop simply returns the list"
+      (let* ([ noop-list '(() ()) ]
+             [ result (diSimp noop-list) ])
+        (check-true (list? noop-list))
+        (check-true (list? result))
+        (check-equal? noop-list result)
+      )
+    )
+
+    (test-case "pop takes the top off the list"
+      (let* ([ pop-list '(() ()) ]
+             [ result (diSimp noop-list) ])
+        (check-true (list? noop-list))
     )
   )
 )
