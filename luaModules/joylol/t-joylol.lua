04618021ee725b6e2e4b194d5b3462a6ec5c97b4
Stephen Gaito <stephen@perceptisys.co.uk>
2017-02-12 17:22:02 +0000

    sketched out how to load a user written ConTeXt module

---
+++ luaModules/joylol/t-joylol.lua
@@ -0,0 +1,14 @@
+if not modules then modules = { } end modules ['t-joylol'] = {
+    version   = 1.000,
+    comment   = "JoyLoL",
+    author    = "PerceptiSys Ltd (Stephen Gaito)",
+    copyright = "PerceptiSys Ltd (Stephen Gaito)",
+    license   = "GNU General Public License"
+}
+
+thirddata        = thirddata        or {}
+thirddata.joylol = thirddata.joylol or {}
+
+local joylol   = thirddata.joylol
+
+texio.write("loaded joylol")
