3d91664192935e4328f3743057d5c8592d94cc7a
Stephen Gaito <stephen@perceptisys.co.uk>
2017-03-07 16:27:56 +0000

    integrated bin scripts for ConTeXt standalone installations

---
+++ bin/context.all.sh
@@ -0,0 +1,14 @@
+
+# Introduce a number of helper functions for using ConTeXt standalone
+
+function uc-b  {
+  source /usr/local/context/beta/tex/setuptex
+}
+
+function uc-c  {
+  source /usr/local/context/current/tex/setuptex
+}
+
+function uc-e  {
+  source /usr/local/context/experimental/tex/setuptex
+}
