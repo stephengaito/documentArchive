fd2542c400793314bc7cc89575c6847c0c01e7b8
Stephen Gaito <stephen@perceptisys.co.uk>
2017-03-20 06:45:17 +0000

    continued work on cuTest lakefile

---
+++ ansi-c/specs/unit/cuTest.need.lua
@@ -0,0 +1,4 @@
+--- variables for package cuTest
+CUTEST_INCLUDE_DIR = 'NIL' --> please set!
+CUTEST_LIBS = 'cuTest' --> please set!
+----
--- ansi-c/specs/unit/lakefile.lua
+++ ansi-c/specs/unit/lakefile.lua
@@ -1,8 +1,32 @@
 -- A Lua Lakefile
 
 local cuTest = dofile '../cuTest/CuTest.lua'
+local lfs = require 'lfs'
+local pp = require 'pl.pretty'
 
 local allTests = cuTest.allTests('Unit')
 local libJoyLoL = '../../lib/libJoyLoL.so'
 
-local defaultTarget = target(allTests['a'],  )
+local specFiles = { }
+local objFiles = { }
+for aFile in lfs.dir('.') do
+  if aFile and aFile:match('%.specs$') then
+    specFiles[#specFiles+1] = aFile
+    objFiles[#objFiles+1]   = aFile:gsub('%.specs$', '-suite.o')
+  end
+end
+print(pp.write(specFiles))
+objFiles[#objFiles+1] = '../cuTest/CuTest.o'
+objFiles[#objFiles+1] = libJoyLoL
+objFiles[#objFiles+1] = allTests['o']
+print(pp.write(objFiles))
+
+rule('.suite', '.specs', function(r)
+  print(pp.write(r))
+end)
+
+rule('.o', '.suite', function(r)
+  print(pp.write(r))
+end)
+
+local defaultTarget = c.program{allTests['a'], src='*.specs', needs='cuTest joyLoL' }
