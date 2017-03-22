31d2aa96c3ed25d1af748a9dc6f1bbe72615c03f
Stephen Gaito <stephen@perceptisys.co.uk>
2017-03-22 20:29:48 +0000

    initial integration of ansi-c gitVersion.h into joyLoLC (need to check index)

--- ansi-c/lib/joyLoLLua.c
+++ ansi-c/lib/joyLoLLua.c
@@ -5,6 +5,24 @@
 #include "lauxlib.h"
 #include "joyLoL.h"
 
+#include "version.h"
+#include "gitVersion.h"
+
+static int luaCVersion (lua_State *L) {
+  lua_pushstring (L, JOYLOLC_VERSION);
+  return 1;
+}
+
+static int luaCGitVersion (lua_State *L) {
+  lua_createtable (L, 0, 10);
+  for ( int i = 0; gitKeyValues[i].key; i++ ) {
+    lua_pushstring(L, gitKeyValues[i].key);
+    lua_pushstring(L, gitKeyValues[i].value);
+    lua_settable(L, -1);
+  }
+  return 1;
+}
+
 static int luaHelloWorld (lua_State *L) {
   const char *aName = luaL_checkstring(L, 1);
   const char *aResponse = helloWorld(aName);
@@ -14,8 +32,10 @@ static int luaHelloWorld (lua_State *L) {
 }
 
 static const struct luaL_Reg joyLoLFuncs [] = {
-  {"helloWorld", luaHelloWorld},
-  {NULL, NULL} /* sentinel */
+  {"cVersion",    luaCVersion},
+  {"cGitVersion", luaCGitVersion},
+  {"helloWorld",  luaHelloWorld},
+  {NULL,          NULL} /* sentinel */
 };
 
 int luaopen_joyLoLC (lua_State *L) {
---
+++ lua/lib/version.lua
@@ -0,0 +1,5 @@
+-- A Lua file
+
+-- declare the version of the JoyLoL (Lua) module
+
+return "joyLoL version 0.1"
