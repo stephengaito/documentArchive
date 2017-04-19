471464c58beca3e2a8e9ff7222de9d58aa2119be
Stephen Gaito <stephen@perceptisys.co.uk>
2017-04-19 08:44:48 +0100

    continued work

--- t-joylol-coalg/tex/context/third/joyLoLCoAlg/t-joylol-coalg.lua
+++ t-joylol-coalg/tex/context/third/joyLoLCoAlg/t-joylol-coalg.lua
@@ -1,1 +1,18 @@
 -- A Lua file
+
+if not modules then modules = { } end modules ['t-joylol-coalg'] = {
+    version   = 1.000,
+    comment   = "JoyLoLCoAlgs",
+    author    = "PerceptiSys Ltd (Stephen Gaito)",
+    copyright = "PerceptiSys Ltd (Stephen Gaito)",
+    license   = "MIT License"
+}
+
+thirddata              = thirddata              or {}
+thirddata.joyLoLCoAlgs = thirddata.joyLoLCoAlgs or {}
+
+local coAlgs     = thirddata.joyLoLCoAlgs
+
+function 
+
+texio.write_nl("loaded JoyLoL CoAlgs\n")
