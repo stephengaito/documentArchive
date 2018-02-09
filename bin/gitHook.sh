a8989fd09b0cdd691913e3a143704a04d89f501e
Stephen Gaito <stephen@perceptisys.co.uk>
2018-02-09 18:01:19 +0000

    continued work on joylolTests

--- bin/gitHook.sh
+++ bin/gitHook.sh
@@ -2,6 +2,22 @@
 #
 # Post-{commit, checkout, merge} hook to set version infomation
 
+# Create the JoyLoL gitVersion-joylol.tex file
+#
+gitVJoylol=bin/gitVersion-joylol.tex
+#
+git log -1 --date=short \
+  --pretty=format:"
+\startJoylolCode
+;;  authorName      = \"%an\",
+;;  commitDate      = \"%ad\",
+;;  commitShortHash = \"%h\",
+;;  commitLongHash  = \"%H\",
+;;  subject         = \"%s\",
+''  notes           = \"%N\"
+\stopJoylolCode
+" HEAD > $gitVJoylol
+
 # Create the ANSI-C gitVersion-c.tex file
 #
 gitVC=bin/gitVersion-c.tex
@@ -57,67 +73,68 @@ git log -1 --date=short \
 
 # Now distribute these files to appropriate locations
 #
-cp $gitVTex module/t-joylol-coalg/doc/context/third/joyLoLCoAlg
-cp $gitVLua module/t-joylol-coalg/doc/context/third/joyLoLCoAlg
-cp $gitVC   module/t-joylol-coalg/doc/context/third/joyLoLCoAlg
+cp $gitVTex    module/t-joylol-coalg/doc/context/third/joyLoLCoAlg
+cp $gitVLua    module/t-joylol-coalg/doc/context/third/joyLoLCoAlg
+cp $gitVC      module/t-joylol-coalg/doc/context/third/joyLoLCoAlg
 
-cp $gitVTex core/context/joylol/t-joylol/doc/context/third/joyLoL
-cp $gitVLua core/context/joylol/t-joylol/doc/context/third/joyLoL
-cp $gitVC   core/context/joylol/t-joylol/doc/context/third/joyLoL
+cp $gitVTex    core/context/joylol/t-joylol/doc/context/third/joyLoL
+cp $gitVLua    core/context/joylol/t-joylol/doc/context/third/joyLoL
+cp $gitVC      core/context/joylol/t-joylol/doc/context/third/joyLoL
 
-cp $gitVTex core/context/options/t-joylol-opts/doc/context/third/joyLoLOpts
-cp $gitVLua core/context/options/t-joylol-opts/doc/context/third/joyLoLOpts
-cp $gitVC   core/context/options/t-joylol-opts/doc/context/third/joyLoLOpts
+cp $gitVTex    core/context/options/t-joylol-opts/doc/context/third/joyLoLOpts
+cp $gitVLua    core/context/options/t-joylol-opts/doc/context/third/joyLoLOpts
+cp $gitVC      core/context/options/t-joylol-opts/doc/context/third/joyLoLOpts
 
-cp $gitVTex core/lua/doc
-cp $gitVLua core/lua/doc
-cp $gitVC   core/lua/doc
+cp $gitVTex    core/lua/doc
+cp $gitVLua    core/lua/doc
+cp $gitVC      core/lua/doc
 
-cp $gitVTex core/textadept/doc
-cp $gitVLua core/textadept/doc
-cp $gitVC   core/textadept/doc
+cp $gitVTex    core/textadept/doc
+cp $gitVLua    core/textadept/doc
+cp $gitVC      core/textadept/doc
 
-cp $gitVTex base/booleans/doc
-cp $gitVC   base/booleans/doc
-cp $gitVTex base/cFunctions/doc
-cp $gitVC   base/cFunctions/doc
-cp $gitVTex base/coAlgebras/doc
-cp $gitVC   base/coAlgebras/doc
-cp $gitVTex base/contexts/doc
-cp $gitVC   base/contexts/doc
-cp $gitVTex base/crossCompilers/doc
-cp $gitVC   base/crossCompilers/doc
-cp $gitVTex base/dictNodes/doc
-cp $gitVC   base/dictNodes/doc
-cp $gitVTex base/dictionaries/doc
-cp $gitVC   base/dictionaries/doc
-cp $gitVTex base/fragments/doc
-cp $gitVC   base/fragments/doc
-cp $gitVTex base/implementations/doc
-cp $gitVC   base/implementations/doc
-cp $gitVTex base/jInterps/doc
-cp $gitVC   base/jInterps/doc
-cp $gitVTex base/loaders/doc
-cp $gitVC   base/loaders/doc
-cp $gitVTex base/luaFunctions/doc
-cp $gitVC   base/luaFunctions/doc
-cp $gitVTex base/naturals/doc
-cp $gitVC   base/naturals/doc
-cp $gitVTex base/pairs/doc
-cp $gitVC   base/pairs/doc
-cp $gitVTex base/parsers/doc
-cp $gitVC   base/parsers/doc
-cp $gitVTex base/rules/doc
-cp $gitVC   base/rules/doc
-cp $gitVTex base/signals/doc
-cp $gitVC   base/signals/doc
-cp $gitVTex base/stringBuffers/doc
-cp $gitVC   base/stringBuffers/doc
-cp $gitVTex base/symbols/doc
-cp $gitVC   base/symbols/doc
-cp $gitVTex base/templates/doc
-cp $gitVC   base/templates/doc
-cp $gitVTex base/texts/doc
-cp $gitVC   base/texts/doc
-cp $gitVTex extensions/joylolTests/doc
-cp $gitVC   extensions/joylolTests/doc
+cp $gitVTex    base/booleans/doc
+cp $gitVC      base/booleans/doc
+cp $gitVTex    base/cFunctions/doc
+cp $gitVC      base/cFunctions/doc
+cp $gitVTex    base/coAlgebras/doc
+cp $gitVC      base/coAlgebras/doc
+cp $gitVTex    base/contexts/doc
+cp $gitVC      base/contexts/doc
+cp $gitVTex    base/crossCompilers/doc
+cp $gitVC      base/crossCompilers/doc
+cp $gitVTex    base/dictNodes/doc
+cp $gitVC      base/dictNodes/doc
+cp $gitVTex    base/dictionaries/doc
+cp $gitVC      base/dictionaries/doc
+cp $gitVTex    base/fragments/doc
+cp $gitVC      base/fragments/doc
+cp $gitVTex    base/implementations/doc
+cp $gitVC      base/implementations/doc
+cp $gitVTex    base/jInterps/doc
+cp $gitVC      base/jInterps/doc
+cp $gitVTex    base/loaders/doc
+cp $gitVC      base/loaders/doc
+cp $gitVTex    base/luaFunctions/doc
+cp $gitVC      base/luaFunctions/doc
+cp $gitVTex    base/naturals/doc
+cp $gitVC      base/naturals/doc
+cp $gitVTex    base/pairs/doc
+cp $gitVC      base/pairs/doc
+cp $gitVTex    base/parsers/doc
+cp $gitVC      base/parsers/doc
+cp $gitVTex    base/rules/doc
+cp $gitVC      base/rules/doc
+cp $gitVTex    base/signals/doc
+cp $gitVC      base/signals/doc
+cp $gitVTex    base/stringBuffers/doc
+cp $gitVC      base/stringBuffers/doc
+cp $gitVTex    base/symbols/doc
+cp $gitVC      base/symbols/doc
+cp $gitVTex    base/templates/doc
+cp $gitVC      base/templates/doc
+cp $gitVTex    base/texts/doc
+cp $gitVC      base/texts/doc
+cp $gitVTex    extensions/joylolTests/doc
+cp $gitVC      extensions/joylolTests/doc
+cp $gitVJoylol extensions/joylolTests/doc
