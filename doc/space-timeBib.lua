579ce1c5a9fa15122e38902e3bca24f89e07abe9
Stephen Gaito <stephen@perceptisys.co.uk>
2019-06-03 12:29:40 +0100

    added citation for moon in a dewdrop

---
+++ doc/space-timeBib.lua
@@ -0,0 +1,13 @@
+return {
+  dogen1985moonInADewDrop = {
+    author = "Dogen, Eihei",
+    publisher = "North Point Press",
+    editor = "Tanahashi, Kazuaki",
+    category = "book",
+    isbn = "978-0-86547-186-3",
+    url = "https://us.macmillan.com/books/9780865471863",
+    tag = "dogen1985moonInADewDrop",
+    title = "moon in a dewdrop: writing of zen master dōgen",
+    year = "1985",
+    },
+  }
